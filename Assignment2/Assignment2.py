6dd73151cb5688b13b8a35c6a024685199c129be
Jinne123 <70944114+Jinne123@users.noreply.github.com>
2025-06-03 14:43:07 +0200

    push

--- Assignment2/Assignment2.py
+++ Assignment2/Assignment2.py
@@ -26,23 +26,20 @@ df['Polio'] = df['Polio'].fillna(df['Polio'].mean())
 df['Total expenditure'] = df['Total expenditure'].fillna(df.groupby('Country')['Total expenditure'].transform('mean'))
 df['Total expenditure'] = df['Total expenditure'].fillna(df['Total expenditure'].mean())
 
-
 # drop deze want thinness 1-19 en 5-9 years missen allebei
 df = df.dropna(subset=['thinness  1-19 years'])
 # drop deze want alle nan waardes zijn ook nan bij schooling, dus te weinig zeggende informatie
 df = df.dropna(subset=['Income composition of resources'])
-
-print(df[df['thinness  1-19 years'].isna()])
-print(df.isnull().sum())
 
 # Multicollineariteit verwijderen
 # corr = df.corr()
 # sns.heatmap(corr, annot=True, cmap='coolwarm')
 # plt.show()
 to_remove = ['Under-five deaths', 'GDP', 'thinness 5-9 years', 'Schooling']
-# population weg, want veel nan en weinig invloed
+# population weg, want veel nan en weinig invloed op y
 to_remove2 = ['Population']
 df = df.drop(columns=to_remove)
+df = df.drop(columns=to_remove2)
 
 # Split de data in man en women
 df_man = df.copy()
@@ -53,11 +50,16 @@ df_woman = df.copy()
 cols_to_drop = [col for col in df.columns if '(men)' in col]
 df_woman = df_woman.drop(columns=cols_to_drop)
 
-# Data splitten
+# Data splitten men
 X_men = df_man.drop(columns='Life expectancy (men)').copy()
 y_men = df_man['Life expectancy (men)']
 Xmen_train, Xmen_test, ymen_train, ymen_test = train_test_split(X_men, y_men, test_size=0.2, random_state=42)
 
+# Data splitten women
 X_women = df_woman.drop(columns='Life expectancy(women)').copy()
 y_women = df_woman['Life expectancy(women)']
 Xwomen_train, Xwomen_test, ywomen_train, ywomen_test = train_test_split(X_women, y_women, test_size=0.2, random_state=42)
+
+
+numeric_cols = df.select_dtypes(include=['number']).columns
+
